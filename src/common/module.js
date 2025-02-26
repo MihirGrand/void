a37dbbe3720546cb1833b517bcfe485ddd33e0a2
Mihir <118926177+MihirGrand@users.noreply.github.com>
2025-02-26 09:50:17 +0530

    Don't need crypto

--- src/common/module.js
+++ src/common/module.js
@@ -1,17 +1,17 @@
 const components = {
-  'search-bar': Search,
-  'todo-list': Todo,
-  'status-bar': Statusbar,
-  'crypto-rate': Crypto,
+  "search-bar": Search,
+  "todo-list": Todo,
+  "status-bar": Statusbar,
+  /*'crypto-rate': Crypto,
   'crypto-popup': CryptoPopup,
-  'crypto-diff': CryptoDiff,
-  'current-time': Clock,
-  'weather-forecast': Weather,
-  'tabs-list': Tabs,
-  'modal-popup': Modal
+  'crypto-diff': CryptoDiff,*/
+  "current-time": Clock,
+  "weather-forecast": Weather,
+  "tabs-list": Tabs,
+  "modal-popup": Modal,
 };
 
-Object.keys(components).forEach(componentName => {
+Object.keys(components).forEach((componentName) => {
   if (!CONFIG.disabled.includes(componentName))
     customElements.define(componentName, components[componentName]);
 });
